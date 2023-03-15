535f0fa986b2250aec1beec4ea7fab0216046dff
Jimmy Wennlund <jimmy@wennlund.nu>
2023-03-15 10:37:57 +0000

    Add defaults to the config struct

--- components/ppp_link/ppp_link.h
+++ components/ppp_link/ppp_link.h
@@ -5,6 +5,8 @@
 
 #include "driver/uart.h"
 
+#include "hal/gpio_types.h"
+
 struct ppp_link_config_s {
     enum {
         PPP_LINK_CLIENT,
@@ -15,10 +17,10 @@ struct ppp_link_config_s {
     uart_port_t uart;
     uart_config_t uart_config;
     struct {
-        int tx;
-        int rx;
-        int rts;
-        int cts;
+        gpio_num_t tx;
+        gpio_num_t rx;
+        gpio_num_t rts;
+        gpio_num_t cts;
     } io;
     struct {
         int rx_buffer_size;
@@ -37,6 +39,33 @@ struct ppp_link_config_s {
     } ppp_server;
 #endif
 };
+
+// clang-format off
+#define PPP_LINK_CFG_DEFAULT() {                    \
+    .type = PPP_LINK_CLIENT,                        \
+    .uart = UART_NUM_NC,                            \
+    .uart_config = {                                \
+        .baud_rate = 115200,                        \
+        .data_bits = UART_DATA_8_BITS,              \
+        .parity = UART_PARITY_DISABLE,              \
+        .stop_bits = UART_STOP_BITS_1,              \
+        .source_clk = UART_CLK,                     \
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,      \
+        .rx_flow_ctrl_thresh = UART_FIFO_LEN - 8,   \
+    },                                              \
+    .io = {                                         \
+        .tx = GPIO_NUM_NC,                          \
+        .rx = GPIO_NUM_NC,                          \
+        .rts = UART_PIN_NO_CHANGE,                  \
+        .cts = UART_PIN_NO_CHANGE,                  \
+    },                                              \
+    .buffer = {                                     \
+        .rx_buffer_size = 2048,                     \
+        .tx_buffer_size = 2048,                     \
+        .rx_queue_size = 30,                        \
+    },                                              \
+};
+// clang-format on
 
 typedef struct ppp_link_config_s ppp_link_config_t;
 
